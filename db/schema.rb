3aa5c07743346bb1dd60433e6c235e505fda2849
margdaly <margaret.daly@gmail.com>
2023-11-14 14:06:15 -0700

    Create pets table

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,18 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_14_021956) do
+
+ActiveRecord::Schema[7.0].define(version: 2023_11_14_210308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.boolean "adoptable", default: true
+    t.string "breed", limit: 4
+    t.string "name"
+    t.bigint "shelter_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.boolean "foster_program"
@@ -22,4 +22,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_14_021956) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pets", "shelters"
 end
