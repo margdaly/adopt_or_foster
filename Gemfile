4440f429d4903d36a4c7603f1325878517342ebc
margdaly <margaret.daly@gmail.com>
2023-11-14 19:16:34 -0700

    feat: Add service to get cat breed info

--- Gemfile
+++ Gemfile
@@ -64,7 +64,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
   gem 'simplecov'
-  # gem 'vcr'
+  gem 'vcr'
   gem 'webmock'
 end
 
---
+++ app/services/cat_service.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Purpose: To make a call to the cat api and return the data
+class CatService
+  def all_cat_breeds
+    get_url('breeds')
+  end
+
+  private
+
+  def conn
+    Faraday.new('https://api.thecatapi.com/v1/') do |f|
+      f.headers['x-api-key'] = ENV['CAT_API_KEY']
+    end
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
