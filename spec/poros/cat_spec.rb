0199c9ff8274069ac4297861235f00d9ccff3891
margdaly <margaret.daly@gmail.com>
2023-11-24 19:20:11 -0700

    test: Add Cat object initialize test

---
+++ spec/poros/cat_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Cat Object' do
+  it 'exists and has attributes' do
+    data = {  breed_id: 'abys',
+              breed_name: 'Abyssinian',
+              temperament: 'Active, Energetic, Independent, Intelligent, Gentle',
+              origin: 'Egypt',
+              description: 'The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.',
+              life_span: '14 - 15' }
+
+    cat = Cat.new(data)
+
+    expect(cat).to be_a(Cat)
+    expect(cat.breed_id).to eq('abys')
+    expect(cat.breed_name).to eq('Abyssinian')
+    expect(cat.temperament).to eq('Active, Energetic, Independent, Intelligent, Gentle')
+    expect(cat.origin).to eq('Egypt')
+    expect(cat.description).to eq("The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.")
+    expect(cat.life_span).to eq('14 - 15')
+  end
+end
