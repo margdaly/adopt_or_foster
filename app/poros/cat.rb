da743db183a75c91302499e7116af77b10068c27
margdaly <margaret.daly@gmail.com>
2024-01-30 15:33:54 -0700

    refactor: Update cat create to only breed name and id

--- app/poros/cat.rb
+++ app/poros/cat.rb
@@ -2,18 +2,10 @@
 
 class Cat
   attr_reader :breed_id,
-              :breed_name,
-              :temperament,
-              :origin,
-              :description,
-              :life_span
+              :breed_name
 
   def initialize(cat_data)
     @breed_id = cat_data[:id]
     @breed_name = cat_data[:name]
-    @temperament = cat_data[:temperament]
-    @origin = cat_data[:origin]
-    @description = cat_data[:description]
-    @life_span = cat_data[:life_span]
   end
 end
